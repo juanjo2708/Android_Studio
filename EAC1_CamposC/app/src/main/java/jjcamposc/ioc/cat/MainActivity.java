28f2e2da15dc2a391656f25c2b1f96cdb9de45d8
juanjo2708 <juanjo2708@gmail.com>
2021-09-29 12:19:20 +0200

    first commit

---
+++ EAC1_CamposC/app/src/main/java/jjcamposc/ioc/cat/MainActivity.java
@@ -0,0 +1,17 @@
+package jjcamposc.ioc.cat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Info","Soc Juan Jose Campos ");
+
+    }
+}
