20c1cc01a69c4578e228cb48b9684d310826b759
juanjo2708 <juanjo2708@gmail.com>
2021-10-13 20:16:56 +0200

    twoActivities_10

--- TwoActivities_10/app/src/main/java/camposc/ioc/twoactivities_10/MainActivity.java
+++ TwoActivities_10/app/src/main/java/camposc/ioc/twoactivities_10/MainActivity.java
@@ -1,9 +1,14 @@
 package camposc.ioc.twoactivities_10;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MISSATGE = "camposc.ioc.twoactivities_10.MISSATGE";
     public final static String EXTRA_CONTINGUT = "camposc.ioc.twoactivities_10.extra.CONTINGUT";
 
+    public final static String TAG ="MainActivity";
+
     // Request codes
     public final static int MISSATGE_RETORNAT =1;
 
@@ -32,6 +39,27 @@ public class MainActivity extends AppCompatActivity {
         etMain = findViewById(R.id.eTMain);
         buttonMain = findViewById(R.id.buttonMain);
         buttoMainRetorn = findViewById(R.id.button2Main);
+
+        //launcher per recollir les dades  retornades de Second Activity
+        ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d(TAG,"onActivityResult: ");
+                        if (result.getResultCode()==78){
+                            Intent intent = result.getData();
+
+                            if(intent !=null){
+                                String dades = intent.getStringExtra("result");
+                                tvMain.setText(dades);
+                            }
+                        }
+                    }
+                }
+        );
+
+
 
         buttonMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,7 +75,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intentMainReturn = new Intent(MainActivity.this,SecondActivity.class);
-                startActivityForResult(intentMainReturn, MISSATGE_RETORNAT);
+                intentMainReturn.putExtra(EXTRA_MISSATGE,tvMain.getText().toString());
+                intentMainReturn.putExtra(EXTRA_CONTINGUT,etMain.getText().toString());
+                setResult(74, intentMainReturn);
+                activityLauncher.launch(intentMainReturn);
+
             }
         });
 
